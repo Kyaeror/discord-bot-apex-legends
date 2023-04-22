fbf1d6f3e264ae82dced19de43fca9f848853f57
Brian Chiang <kyaerorgabootcamp@gmail.com>
2023-04-22 01:23:22 -0400

    weather api implementation

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ app.get(`/`, (req,res)=>{
 })
 
 
-var initialCommand = `!`
+var initialCommand = `~`
 const helpMenu = `
 normal -- shows current/next map and time remaining for normal game modes. 
 ranked -- shows current/next map and time remaining for ranked.
-changeCommand -- changes initial symbol for commands. (Will always reset after I restart computer or turn off application)
+changeCommand -- changes initial symbol for commands.
 `
 
 const client = new Client({
@@ -28,7 +28,7 @@ client.on(`ready`, ()=> {
     console.log(`Logged in as ${client.user.tag}`)
 })
 
-client.on(`messageCreate`, (message) => {
+client.on(`messageCreate`, async (message) => {
     if (message.content.startsWith(initialCommand)){
         //change the initial symbol for commands
         if(message.content.endsWith(`changeCommand`)){
@@ -79,6 +79,11 @@ client.on(`messageCreate`, (message) => {
         if(message.content.endsWith(`help`)){
             message.reply(helpMenu)
         }
+
+
+        if(message.content.endsWith(`notifyNormal`)){
+            
+        }
         //add notify when map change
             //get user.id
             //fetch data timer
@@ -86,6 +91,64 @@ client.on(`messageCreate`, (message) => {
             //set timer when reach timer then message.reply to user.id
 
 
+
+
+            if(message.content.endsWith(`weather`)){
+                message.reply(`What city were you looking for?`)
+                    .then(()=> {
+                        const filter = (msg) => msg.author.id === message.author.id
+                        const collector = message.channel.createMessageCollector(filter, {
+                            max: 1,
+                            time: 60000
+                        })
+                        collector.on(`collect`, (message)=>{
+                            
+                                fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${message.content}&limit=1&appid=${process.env.WEATHER_API_KEY}`)
+                                    .then(res => res.json())
+                                    .then((data)=>{
+                                        const lat = data[0].lat
+                                        const lon = data[0].lon
+                                        message.reply(`You want to know the weather for ${data[0].name} in ${data[0].country} correct? type only yes or no`)
+                                            .then(()=>{
+                                                const filter = (msg) => msg.author.id === message.author.id
+                                                const collector = message.channel.createMessageCollector(filter, {
+                                                    max: 1,
+                                                    time: 60000
+                                                })
+                    
+                                                collector.on(`collect`, (message)=>{
+                                                    if(message.content == `yes`){
+                                                        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=imperial`)
+                                                            .then(res=> res.json())
+                                                            .then((data)=> {
+                                                                message.reply(`It's expected to be ${data.main.temp} fahrenheit, ${data.weather[0].description}, ${data.main.humidity}% humidity, ${data.wind.speed}MPH wind speeds.`)
+                                                            })
+                                                        collector.stop()
+                                                    }else{
+                                                        message.reply(`Sorry then please run the command again and type the correct city`)
+                                                        collector.stop()
+                                                    }
+                                                })
+
+                                                collector.on(`end`, (collected, reason)=>{
+                                                     if(reason ===`time`){
+                                                        message.reply(`Command change timed out.`)
+                                                    }
+                                                 })
+                                            })
+                                    })
+                
+                            
+                            collector.stop()
+                        })
+
+                        collector.on(`end`, (collected, reason)=>{
+                            if(reason ===`time`){
+                                message.reply(`Command change timed out.`)
+                            }
+                        })
+                    })
+            }
     }
 })
 
